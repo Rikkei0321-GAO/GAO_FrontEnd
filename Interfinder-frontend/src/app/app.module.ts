bef2233bb800b08ce76a5ae63121b55105c5e096
ThanhHaiii <thanhhaiphan111@gmail.com>
2021-04-01 17:18:51 +0700

    trang-chu

--- Interfinder-frontend/src/app/app.module.ts
+++ Interfinder-frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
+// @ts-ignore
 import { NgModule } from '@angular/core';
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// @ts-ignore
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {AdminModule} from './admin/admin.module';
+import {QuanlytaikhoanIndexComponent} from './admin/quanlytaikhoan-index/quanlytaikhoan-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    QuanlytaikhoanIndexComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
